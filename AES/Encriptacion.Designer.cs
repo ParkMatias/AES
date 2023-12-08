b11cb2cf9b75b30297efb5421e75085392f773df
ParkMatias <83047050+ParkMatias@users.noreply.github.com>
2023-12-07 21:36:11 -0300

    init

---
+++ AES/Encriptacion.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace AES
+{
+    partial class Encriptacion
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxMensaje = new System.Windows.Forms.TextBox();
+            this.textBoxEncriptado = new System.Windows.Forms.TextBox();
+            this.btnEncriptar = new System.Windows.Forms.Button();
+            this.btnDesencriptar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxMensaje
+            // 
+            this.textBoxMensaje.Location = new System.Drawing.Point(178, 126);
+            this.textBoxMensaje.Name = "textBoxMensaje";
+            this.textBoxMensaje.Size = new System.Drawing.Size(212, 20);
+            this.textBoxMensaje.TabIndex = 0;
+            // 
+            // textBoxEncriptado
+            // 
+            this.textBoxEncriptado.Location = new System.Drawing.Point(777, 126);
+            this.textBoxEncriptado.Name = "textBoxEncriptado";
+            this.textBoxEncriptado.Size = new System.Drawing.Size(240, 20);
+            this.textBoxEncriptado.TabIndex = 1;
+            // 
+            // btnEncriptar
+            // 
+            this.btnEncriptar.Location = new System.Drawing.Point(132, 28);
+            this.btnEncriptar.Name = "btnEncriptar";
+            this.btnEncriptar.Size = new System.Drawing.Size(323, 81);
+            this.btnEncriptar.TabIndex = 2;
+            this.btnEncriptar.Text = "Encriptar";
+            this.btnEncriptar.UseVisualStyleBackColor = true;
+            this.btnEncriptar.Click += new System.EventHandler(this.btnEncriptar_Click);
+            // 
+            // btnDesencriptar
+            // 
+            this.btnDesencriptar.Location = new System.Drawing.Point(729, 28);
+            this.btnDesencriptar.Name = "btnDesencriptar";
+            this.btnDesencriptar.Size = new System.Drawing.Size(323, 81);
+            this.btnDesencriptar.TabIndex = 3;
+            this.btnDesencriptar.Text = "Desencriptar";
+            this.btnDesencriptar.UseVisualStyleBackColor = true;
+            this.btnDesencriptar.Click += new System.EventHandler(this.btnDesencriptar_Click);
+            // 
+            // Encriptacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1256, 634);
+            this.Controls.Add(this.btnDesencriptar);
+            this.Controls.Add(this.btnEncriptar);
+            this.Controls.Add(this.textBoxEncriptado);
+            this.Controls.Add(this.textBoxMensaje);
+            this.Name = "Encriptacion";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxMensaje;
+        private System.Windows.Forms.TextBox textBoxEncriptado;
+        private System.Windows.Forms.Button btnEncriptar;
+        private System.Windows.Forms.Button btnDesencriptar;
+    }
+}
+
---
+++ AES/Encriptacion.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+using System.IO;
+using System.Linq;
+
+namespace AES
+{
+    public partial class Encriptacion : Form
+    {
+        private string clave = "";
+
+        public Encriptacion()
+        {
+            InitializeComponent();
+        }
+        private string PedirClave()
+        {
+            string clave = "";
+
+            string input = Microsoft.VisualBasic.Interaction.InputBox("Ingrese la clave:", "Clave", "", -1, -1);
+            if (!string.IsNullOrEmpty(input))
+            {
+                clave = input;
+            }
+
+            return clave;
+        }
+
+        private byte[] DerivarClave(string clave, int keySize)
+        {
+            byte[] keyBytes = new byte[keySize / 8];
+            Array.Copy(Encoding.UTF8.GetBytes(clave), keyBytes, Math.Min(keyBytes.Length, clave.Length));
+            return keyBytes;
+        }
+
+        private void btnEncriptar_Click(object sender, EventArgs e)
+        {
+            string mensaje = textBoxMensaje.Text;
+            clave = PedirClave();
+            if (clave != "")
+            {
+                byte[] mensajeEncriptado = Encriptar(mensaje, clave);
+                textBoxEncriptado.Text = Convert.ToBase64String(mensajeEncriptado);
+            }
+            else
+            {
+                MessageBox.Show("No se ingresó una clave válida.");
+            }
+        }
+
+        private void btnDesencriptar_Click(object sender, EventArgs e)
+        {
+            string mensajeEncriptadoString = textBoxEncriptado.Text;
+            if (clave == "")
+            {
+                MessageBox.Show("Debes encriptar primero para ingresar la clave.");
+                return;
+            }
+
+            string claveDesencriptar = PedirClave();
+            if (claveDesencriptar != clave)
+            {
+                MessageBox.Show("La clave ingresada no coincide con la clave de encriptación.");
+                return;
+            }
+
+            byte[] mensajeEncriptado = Convert.FromBase64String(mensajeEncriptadoString);
+            string mensajeDesencriptado = Desencriptar(mensajeEncriptado, claveDesencriptar);
+            MessageBox.Show("Mensaje desencriptado: " + mensajeDesencriptado);
+        }
+
+        private byte[] Encriptar(string mensaje, string clave)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = DerivarClave(clave, aesAlg.KeySize);
+                aesAlg.GenerateIV();
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        byte[] bytesMensaje = Encoding.UTF8.GetBytes(mensaje);
+                        csEncrypt.Write(bytesMensaje, 0, bytesMensaje.Length);
+                    }
+
+                    byte[] ivAndEncrypted = aesAlg.IV.Concat(msEncrypt.ToArray()).ToArray();
+                    return ivAndEncrypted;
+                }
+            }
+        }
+
+        private string Desencriptar(byte[] mensajeEncriptado, string clave)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = DerivarClave(clave, aesAlg.KeySize);
+                byte[] iv = mensajeEncriptado.Take(16).ToArray();
+                aesAlg.IV = iv;
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(mensajeEncriptado.Skip(16).ToArray()))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            return srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
